9de1040273ed0ce7a7c83171213fe0eb8b490afa
yderek5 <yderek5@gmail.com>
2017-10-08 17:42:03 -0500

    fixed fight multiple enemies bug

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,10 @@
 // when you click a character it moves it to the YOUR CHARACTERS area
 // after you select a character it moves the unselected characters to EMEMIES AVAILABLE
-// you then must select an enemy to attack
+// you then must select an enemy to attack and they move to DEFENDER area
 
 
+$(document).ready(function() {
+    //Display the health of the characters
 var lukeHealthPoints = "120";
 var benHealthPoints = "100";
 var bobaHealthPoints = "100";
@@ -12,23 +14,105 @@ var benAttackPoints = 12;
 var bobaAttackPoints = 8;
 var vaderAttackPoints = 16;
 var isCharacterChosen = false;
-
-$(document).ready(function() {
-    //Display the health of the characters
+var isEnemyChosen = false;
     $(".healthpointsLuke").html(lukeHealthPoints);
     $(".healthpointsBen").html(benHealthPoints);
     $(".healthpointsBoba").html(bobaHealthPoints);
     $(".healthpointsVader").html(vaderHealthPoints);
 
-    //User clicks luke"
+
+    //User clicks luke
+    //check to see if character is chosen
     $("#luke").on("click", function() {
-        isCharacterChosen = true;
-        $("#luke").appendTo("#your-character");
-        $("#ben").appendTo("#enemies-to-attack");
-        $("#ben").addClass("redBorder");
-        $("#boba").appendTo("#enemies-to-attack");
-        $("#boba").addClass("redBorder");
-        $("#vader").appendTo("#enemies-to-attack");
-        $("#vader").addClass("redBorder");
+    if(isEnemyChosen) {
+        alert("You can only fight one at a time!");
+        ("#luke").appendTo("#enemies-to-attack");
+    }
+        if (isCharacterChosen) {
+            isEnemyChosen = true;
+            $("#luke").appendTo("#defender");
+        } else {
+            isCharacterChosen = true;
+            isEnemyChosen = false;
+            $("#luke").appendTo("#your-character");
+            $("#ben").appendTo("#enemies-to-attack");
+            $("#ben").addClass("redBorder");
+            $("#boba").appendTo("#enemies-to-attack");
+            $("#boba").addClass("redBorder");
+            $("#vader").appendTo("#enemies-to-attack");
+            $("#vader").addClass("redBorder");
+        };
     });
+    //User clicks ben
+    //check to see if character is chosen
+    $("#ben").on("click", function() {
+        if(isEnemyChosen) {
+        alert("You can only fight one at a time!");
+        ("#ben").appendTo("#enemies-to-attack");
+    }
+        if (isCharacterChosen) {
+            isEnemyChosen = true;
+            $("#ben").appendTo("#defender");
+        } else {
+            isCharacterChosen = true;
+            $("#ben").appendTo("#your-character");
+            $("#luke").appendTo("#enemies-to-attack");
+            $("#luke").addClass("redBorder");
+            $("#boba").appendTo("#enemies-to-attack");
+            $("#boba").addClass("redBorder");
+            $("#vader").appendTo("#enemies-to-attack");
+            $("#vader").addClass("redBorder");
+        };
+    });
+    //User clicks boba
+    //check to see if character is chosen
+    $("#boba").on("click", function() {
+        if(isEnemyChosen) {
+        alert("You can only fight one at a time!");
+        ("#boba").appendTo("#enemies-to-attack");
+    }
+        if (isCharacterChosen) {
+            isEnemyChosen = true;
+            $("#boba").appendTo("#defender");
+        } else {
+            isCharacterChosen = true;
+            $("#boba").appendTo("#your-character");
+            $("#ben").appendTo("#enemies-to-attack");
+            $("#ben").addClass("redBorder");
+            $("#luke").appendTo("#enemies-to-attack");
+            $("#luke").addClass("redBorder");
+            $("#vader").appendTo("#enemies-to-attack");
+            $("#vader").addClass("redBorder");
+        };
+    });
+    //User clicks vader
+    //check to see if character is chosen
+    $("#vader").on("click", function() {
+        if(isEnemyChosen) {
+        alert("You can only fight one at a time!");
+        ("#vader").appendTo("#enemies-to-attack");
+    }
+        if (isCharacterChosen) {
+            isEnemyChosen = true;
+            $("#vader").appendTo("#defender");
+        } else {
+            isCharacterChosen = true;
+            $("#vader").appendTo("#your-character");
+            $("#ben").appendTo("#enemies-to-attack");
+            $("#ben").addClass("redBorder");
+            $("#boba").appendTo("#enemies-to-attack");
+            $("#boba").addClass("redBorder");
+            $("#luke").appendTo("#enemies-to-attack");
+            $("#luke").addClass("redBorder");
+        };
+    });
+
+
+
+
+
+
+
+
+
 });
